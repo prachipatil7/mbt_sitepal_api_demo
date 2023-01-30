5bb1f0378cc3904260a61e0276d0e362b67d2f64
prachipatil7 <62350419+prachipatil7@users.noreply.github.com>
2023-01-30 13:11:37 -0600

    added some speech functionality

--- script.js
+++ script.js
@@ -7,32 +7,13 @@ function addText(txt){
     document.getElementById("demo_steps").innerHTML += `<p>${txt}</p>`;
    }
 
-function tell_story() {
-    addText('sayText(txt,3,1,3)')
-    let txt = `
-    Christian Blast had always loved urban Chicago with its naughty, numerous nooks. It was a place where he felt unstable. 
-    He was a cute, smelly, wine drinker with ginger fingers and solid arms. His friends saw him as an eggy, elated elephant. 
-    Once, he had even made a cup of tea for a kosher deaf person. That's the sort of man he was.
-    Christian walked over to the window and reflected on his creepy surroundings. The sun shone like rampaging elephants.
-    Then he saw something in the distance, or rather someone. It was the figure of Suzanne McCallister. 
-    Suzanne was a friendly doctor with ugly fingers and handsome arms.
-    Christian gulped. He was not prepared for Suzanne.
-    As Christian stepped outside and Suzanne came closer, he could see the old smile on her face.
-    "Look Christian," growled Suzanne, with a selfish glare that reminded Christian of friendly donkeys. 
-    "It's not that I don't love you, but I want love. You owe me 4288 pounds."
-    Christian looked back, even more anxious and still fingering the damp torch. "Suzanne, get out of my house," he replied.
-    They looked at each other with healthy feelings, like two dry, dirty dogs singing at a very bold bar mitzvah, 
-    which had R & B music playing in the background and two rude uncles eating to the beat.
-    Suddenly, Suzanne lunged forward and tried to punch Christian in the face. 
-    Quickly, Christian grabbed the damp torch and brought it down on Suzanne's skull.
-    Suzanne's ugly fingers trembled and her handsome arms wobbled. 
-    She looked anxious, her wallet raw like a relieved, rapid record.
-    Then she let out an agonising groan and collapsed onto the ground. 
-    Moments later Suzanne McCallister was dead.
-    Christian Blast went back inside and made himself a nice glass of wine.`
-    txt = 'Hello World'
-    sayText(txt,3,1,3)
+function hello_world() {
+    speak_text('Hello World!')
+}
 
+function speak_text(txt) {
+    addText('sayText(txt,3,1,3)')
+    sayText(txt,3,1,3)
 }
 
 function clearDiv() {
