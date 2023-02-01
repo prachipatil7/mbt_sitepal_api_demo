6f64388714ef04fe82f5394d97b08d1f8acb7ba0
prachipatil7 <62350419+prachipatil7@users.noreply.github.com>
2023-02-01 10:08:03 -0600

    added background

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-function start_demo() {
-    tell_story();
-    gaze_control();
-}
 
 function addText(txt){
     document.getElementById("demo_steps").innerHTML += `<p>${txt}</p>`;
@@ -15,6 +11,11 @@ function speak_text(txt) {
     document.getElementById("char_count").innerHTML = `${txt.length} chars`;
     addText('sayText(txt,3,1,3)')
     sayText(txt,3,1,3)
+}
+
+function change_bg() {
+    url = "url(https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/Northwestern_Arch.jpg/800px-Northwestern_Arch.jpg?20050214042121)"
+    document.getElementById("sitepal_window").style.backgroundImage = url
 }
 
 function clearDiv() {
